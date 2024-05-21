e5332533df8cee38e582258ab3901e92a4680971
Andy Nonaka <AJNonaka@lbl.gov>
2024-05-21 11:26:05 -0700

    more cleanup

--- Source/MagneX.cpp
+++ Source/MagneX.cpp
@@ -2,15 +2,21 @@
 
 #include <AMReX_ParmParse.H>
 
+/////////////////////////////////////////////////////
+// Default values for these parameters can be seen in
+// the InitializeMagneXNamespace() function below
+/////////////////////////////////////////////////////
+
 // total number of grid cells in the simulation domain in each direction
 AMREX_GPU_MANAGED amrex::GpuArray<int, 3> MagneX::n_cell;
 
-// maximum grid size in each direction (for spatial domain decomposition)
+// for spatial domain decomposition
+// maximum number of cells in each box in each direction;
 int MagneX::max_grid_size_x;
 int MagneX::max_grid_size_y;
 int MagneX::max_grid_size_z;
 
-// physical lo/hi coordiates
+// physical lo/hi coordiates of the domain
 AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real, 3> MagneX::prob_lo;
 AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real, 3> MagneX::prob_hi;
 
@@ -19,6 +25,9 @@ int MagneX::nsteps;
 
 // maximum simulation time (will override nsteps if reached)
 amrex::Real MagneX::stop_time;
+
+// time step
+amrex::Real MagneX::dt;
 
 // time integration options
 // 1 = first order forward Euler
@@ -33,9 +42,6 @@ int MagneX::TimeIntegratorOption;
 // for TimeIntegrationOption=2, iterative_tolerance=0. means force 2 iterations
 // for TimeIntegrationOption=3, iterative_tolerance=0. means force 1 iteration
 amrex::Real MagneX::iterative_tolerance;
-
-// time step
-amrex::Real MagneX::dt;
 
 // how often to write a plotfile
 int MagneX::plot_int;
@@ -60,7 +66,15 @@ int MagneX::restart;
 // 2 = standard problem 2
 int MagneX::diag_type;
 
+// whether to call the parser each time step, or only at initialization
+int MagneX::timedependent_Hbias;
+int MagneX::timedependent_alpha;
+
+////////////////////////////////
+// parameters for Hbias sweeping
+
 // flag for enabling Hbias sweeps for hysteresis calculations
+// cannot have Hbias_sweep=1 AND timedependent_Hbias=1
 int MagneX::Hbias_sweep;
 
 // tolerance threshold for equilibrium in M in Hbias sweeping
@@ -71,43 +85,44 @@ AMREX_GPU_MANAGED amrex::Real MagneX::increment_size;
 
 // number of increments on Hbias before reversing sign of increment
 int MagneX::nsteps_hysteresis;
+////////////////////////////////
 
 // permeability
 AMREX_GPU_MANAGED amrex::Real MagneX::mu0;
 
-// whether to call the parser each time step, or only at initialization
-int MagneX::timedependent_Hbias;
-int MagneX::timedependent_alpha;
+// 0 = unsaturated; 1 = saturated
+AMREX_GPU_MANAGED int MagneX::M_normalization;
 
 // 0 = disable precession; 1 = enable precession
 AMREX_GPU_MANAGED int MagneX::precession;
 
-// 0 = no demagnetization; 1 = use demanetization
-AMREX_GPU_MANAGED int MagneX::demag_coupling;
-
-// 0 = unsaturated; 1 = saturated
-AMREX_GPU_MANAGED int MagneX::M_normalization;
-
-// turn on exchange
+// 0 = disable exchange; 1 = enable exchange
 AMREX_GPU_MANAGED int MagneX::exchange_coupling;
 
-// turn on DMI
+// 0 = disable DMI; 1 = enable DMI
 AMREX_GPU_MANAGED int MagneX::DMI_coupling;
 
-// turn on anisotropy
+// 0 = disable anisotropy; 1 = enable anisotropy
+// anisotropy_axis only required for anistropy_coupling=1
 AMREX_GPU_MANAGED int MagneX::anisotropy_coupling;
 AMREX_GPU_MANAGED amrex::GpuArray<amrex::Real, 3> MagneX::anisotropy_axis;
 
-// Choose an FFT solver; 0 = FFTW, 1 = heFFTe
+// 0 = no demagnetization; 1 = use demanetization
+AMREX_GPU_MANAGED int MagneX::demag_coupling;
+
+// demagnetization FFT solver liblrary
+// 0 = FFTW (single-MPI), 1 = heFFTe (distributed)
 AMREX_GPU_MANAGED int MagneX::FFT_solver;
 
 void InitializeMagneXNamespace() {
 
     BL_PROFILE_VAR("InitializeMagneXNamespace()",InitializeMagneXNameSpace);
 
+    /////////////////////////////////////////////////////
     // ParmParse is way of reading inputs from the inputs file
     // pp.get means we require the inputs file to have it
     // pp.query means we optionally need the inputs file to have it - but we must supply a default here
+    /////////////////////////////////////////////////////
     ParmParse pp;
 
     // temporary vectors for reading in parameters from inputs files
@@ -139,21 +154,12 @@ void InitializeMagneXNamespace() {
     stop_time = 1.e9;
     pp.query("stop_time",stop_time);
 
-    nsteps_hysteresis = 1000000000;
-    pp.query("nsteps_hysteresis",nsteps_hysteresis);
-
-    equilibrium_tolerance = 1.e-6;
-    pp.query("equilibrium_tolerance",equilibrium_tolerance);
-
-    increment_size = 1.e-5;
-    pp.query("increment_size",increment_size);
+    pp.get("dt",dt);
 
     pp.get("TimeIntegratorOption",TimeIntegratorOption);
 
     iterative_tolerance = 1.e-9;
     pp.query("iterative_tolerance",iterative_tolerance);
-
-    pp.get("dt",dt);
 
     plot_int = -1;
     pp.query("plot_int",plot_int);
@@ -180,24 +186,25 @@ void InitializeMagneXNamespace() {
     diag_type = -1;
     pp.query("diag_type",diag_type);
 
-    Hbias_sweep = 0;
-    pp.query("Hbias_sweep", Hbias_sweep);
-	
-    pp.get("mu0",mu0);
-
     pp.get("timedependent_Hbias",timedependent_Hbias);
     pp.get("timedependent_alpha",timedependent_alpha);
 
+    Hbias_sweep = 0;
+    pp.query("Hbias_sweep", Hbias_sweep);
+
+    if (Hbias_sweep == 1) {
+        pp.get("equilibrium_tolerance",equilibrium_tolerance);
+        pp.get("increment_size",increment_size);
+        pp.get("nsteps_hysteresis",nsteps_hysteresis);
+    }
+
+    pp.get("mu0",mu0);
+
+    pp.get("M_normalization", M_normalization);
+
     precession = 1;
     pp.query("precession",precession);
-   
-    pp.get("demag_coupling",demag_coupling);
 
-    if (demag_coupling == 1) {
-        pp.get("FFT_solver",FFT_solver);
-    }
-        
-    pp.get("M_normalization", M_normalization);
     pp.get("exchange_coupling", exchange_coupling);
     pp.get("DMI_coupling", DMI_coupling);
 
@@ -208,4 +215,19 @@ void InitializeMagneXNamespace() {
             anisotropy_axis[i] = temp[i];
         }
     }
+   
+    pp.get("demag_coupling",demag_coupling);
+    if (demag_coupling == 1) {
+        pp.get("FFT_solver",FFT_solver);
+    }
+
+    // invalid parameter combination checking
+    if (Hbias_sweep == 1 && timedependent_Hbias == 1) {
+        amrex::Abort("Cannot have Hbias_sweep == 1 && timedependent_Hbias == 1");
+    }
+
+    if (diag_type == 2 && Hbias_sweep == 0) {
+        amrex::Abort("Cannot have diag_type == 2 && Hbias_sweep == 0");
+    }
+    
 }
